d4168c9e43de95c6e9789043db885fa96d95a9f4
Pietro Sette <pietrosette@gmail.com>
2020-07-29 13:36:42 +0200

    added random answers

--- js/script.js
+++ js/script.js
@@ -22,7 +22,19 @@ function aggiungi() {
 }
 
 function risposta() {
-            var testo = "ok";
+            var listaRisposte = [
+                "If anyone asks where I am, I’ve left the country.",
+                "If we’re both going crazy, then we’ll go crazy together, right?",
+                "The campaign took two weeks to plan. How was I supposed to know it was going to take ten hours?",
+                "Maybe you thought you were helping, but you weren’t. You hurt me, do you understand? What you did sucks.",
+                "We’re not even in the game; we’re on the bench.",
+                "Hey, if we’re both going crazy, we’ll go crazy together, right?",
+                "Do you want to figure it out?"
+            ]
+
+            var index = randomNumber(1,7);
+            var testo = listaRisposte[index];
+            console.log(testo);
             var d = new Date();
             var messaggio = $('.template .message').clone();
 
@@ -30,4 +42,8 @@ function risposta() {
             messaggio.find('small').append(d.getHours()+':'+d.getMinutes());
             messaggio.addClass('received');
             $('.messages-box').append(messaggio);
+}
+
+function randomNumber (min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
